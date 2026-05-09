a9097b582f4548dcca43f7d3864571e40ac63cc0
Mitchell DeHaven <mitchell.dehaven@outlook.com>
2026-05-08 22:01:46 -0600

    Allow bypassing with IGNORE_DIRTYGIT
    Its annoying af to try to debug code that has the check without commenting it out. This allows bypassing it

--- src/dirtygit/_core.py
+++ src/dirtygit/_core.py
@@ -1,7 +1,21 @@
+import logging
+import os
 import subprocess
+
+logger = logging.getLogger(__name__)
 
 
 def check() -> str:
+    if os.getenv("IGNORE_DIRTYGIT") == "true":
+        logger.warning("WARNING - IGNORE_DIRTYGIT IS TRUE. WILL NOT ASSERT CLEAN GIT STATUS.")
+        result = subprocess.run(
+            ["git", "rev-parse", "HEAD"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        return result.stdout.strip()
+
     status = subprocess.run(
         ["git", "status", "--porcelain"],
         capture_output=True,
